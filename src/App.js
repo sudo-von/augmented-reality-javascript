67f7a95d9e4391314fd339c44f48660b8de1091b
VoN <sudo.von.contact@gmail.com>
2021-04-26 23:03:16 -0500

    feat: :sparkles: Add first model to the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import './App.css'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <model-viewer
+            src="https://modelviewer.dev/examples/loading/../../shared-assets/models/reflective-sphere.gltf"
+            ios-src=".https://modelviewer.dev/examples/loading/../../shared-assets/models/Astronaut.glb"
+            alt='model name'
+            ar
+            loading='lazy'
+            camera-controls
+            autoplay>
+          </model-viewer>
       </div>
     );
   }
