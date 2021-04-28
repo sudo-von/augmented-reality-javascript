f8b5a1c4c69a10b6af16263fbfd1c383961b58fc
VoN <sudo.von.contact@gmail.com>
2021-04-27 21:48:51 -0500

    feat: :sparkles: Add the rest of the assets. Add carousel. Add responsive design.

--- src/pages/AugmentedReality/Components/ModelViewer/ModelViewer.js
+++ src/pages/AugmentedReality/Components/ModelViewer/ModelViewer.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import Loader from '../../../../components/Loader/'
 
-const ModelViewer = ( { src, iossrc, alt } ) => 
-    <model-viewer
-        src={ src ? src : "https://modelviewer.dev/shared-assets/models/reflective-sphere.gltf"}
-        ios-src={ iossrc ? iossrc : "https://modelviewer.dev/shared-assets/models/Astronaut.glb"}
-        alt={alt ? alt : "3D Model"}
-        ar
-        camera-controls
-        auto-rotate
-        autoplay>
-    </model-viewer>
+const styles = {
+    div: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    modelViewer: {
+        height: 350,
+        width: 550
+    }
+}
+
+const ModelViewer = ( { name, src, iossrc, alt } ) => 
+    <div style={styles.div}>
+        <model-viewer
+            style={styles.modelViewer}
+            src={ src ? src : "https://modelviewer.dev/shared-assets/models/reflective-sphere.gltf"}
+            ios-src={ iossrc ? iossrc : "https://modelviewer.dev/shared-assets/models/Astronaut.glb"}
+            alt={alt ? alt : "3D Model"}
+            ar
+            camera-controls
+            auto-rotate
+            autoplay>
+        </model-viewer>
+        <h2>{name}</h2>
+    </div>
 
 export default ModelViewer
--- src/pages/AugmentedReality/index.js
+++ src/pages/AugmentedReality/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 /* Material-ui. */
 import Grid from '@material-ui/core/Grid'
+import Carousel from 'react-material-ui-carousel'
 /* Custom components. */
 import Navbar from '../../components/Navbar/'
 import ModelViewer from './Components/ModelViewer/'
+/* Assets. */
+import androidJelly from '../../assets/android/jelly.gltf'
+import iosJelly from '../../assets/ios/jelly.glb'
+import androidFurniture from '../../assets/android/furniture.gltf'
+import iosFurniture from '../../assets/ios/furniture.glb'
+import androidArmchair from '../../assets/android/armchair.gltf'
+import iosArmchair from '../../assets/ios/armchair.glb'
 
 const styles = {
     grid: {
         container: {
-            height: '100vh'
+            height: '90vh',
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center'
         },
         item: {
             display: 'flex', 
@@ -18,14 +29,42 @@ const styles = {
     }
 }
 
-const AugmentedReality = ( { theme, themeToggler } ) => 
-    <React.Fragment>
-        <Navbar theme={theme} themeToggler={themeToggler}/>
-        <Grid container style={styles.container}>
-            <Grid item xs={12} style={styles.item}>
-                <ModelViewer/>
+const AugmentedReality = ( { theme, themeToggler } ) => {
+
+    /* Hook. */
+    const list = [
+        {
+            name: "Mesa con gelatina", 
+            android: androidJelly,
+            ios: iosJelly
+        },
+        {
+            name: "Estante con televisión", 
+            android: androidFurniture,
+            ios: iosFurniture
+        },
+        {
+            name: "Sillón", 
+            android: androidArmchair,
+            ios: iosArmchair
+        },
+    ]
+    const [assets, _ ] = useState(list)
+
+    return(
+        <React.Fragment>
+            <Navbar theme={theme} themeToggler={themeToggler}/>
+            <Grid container style={styles.grid.container}>
+                <Grid xs={12} item style={styles.grid.item}>
+                    <Carousel swipe={false} autoPlay={false} navButtonsAlwaysVisible={true}>
+                        {
+                            assets.map( (asset, i) => <ModelViewer key={i} name={asset.name} src={asset.android} iossrc={asset.ios}/> )
+                        }
+                    </Carousel>
+                </Grid>
             </Grid>
-        </Grid>
-    </React.Fragment>    
+        </React.Fragment>    
+    )
+}
 
 export default AugmentedReality
