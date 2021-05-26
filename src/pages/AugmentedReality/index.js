a5776d939f850abc36cc788dddcf15e4c7ebd78f
VoN <sudo.von.contact@gmail.com>
2021-05-25 22:46:22 -0500

    feat: :construction: The catalogue won't be hardcoded, instead it will be in a public json file that will be consumed by the component via require/custom hook.

--- src/pages/AugmentedReality/index.js
+++ src/pages/AugmentedReality/index.js
@@ -1,69 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 /* Material-ui. */
-import Grid from '@material-ui/core/Grid'
-import Carousel from 'react-material-ui-carousel'
+import Grid from "@material-ui/core/Grid"
+import Carousel from "react-material-ui-carousel"
 /* Custom components. */
-import Navbar from '../../components/Navbar/'
-import ModelViewer from './Components/ModelViewer/'
-/* Assets. */
-import androidJelly from '../../assets/android/jelly.gltf'
-import iosJelly from '../../assets/ios/jelly.glb'
-import androidFurniture from '../../assets/android/furniture.gltf'
-import iosFurniture from '../../assets/ios/furniture.glb'
-import androidArmchair from '../../assets/android/armchair.gltf'
-import iosArmchair from '../../assets/ios/armchair.glb'
+import Navbar from "../../components/Navbar/"
+import ModelViewer from "./Components/ModelViewer/"
+import StoreDescription from "./Components/StoreDescription/"
+import StoreServices from "./Components/StoreServices/"
+import Advantages from "./Components/Advantages/"
+import Catalogue from "./Components/Catalogue/"
 
 const styles = {
     grid: {
         container: {
-            height: '90vh',
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center'
+            background: "red",
         },
         item: {
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center'
+            h1: {
+                textAlign: "center"
+            },
+            textAlign: "left"
         }
     }
 }
 
 const AugmentedReality = ( { theme, themeToggler } ) => {
 
-    /* Hook. */
-    const list = [
-        {
-            name: "Mesa con gelatina", 
-            android: androidJelly,
-            ios: iosJelly
-        },
-        {
-            name: "Estante con televisión", 
-            android: androidFurniture,
-            ios: iosFurniture
-        },
-        {
-            name: "Sillón", 
-            android: androidArmchair,
-            ios: iosArmchair
-        },
-    ]
-    const [assets, _ ] = useState(list)
+    /* Custom hook. */
+    const catalogue = require('../../data/catalogue.json')
+    const [furniture, _ ] = useState(catalogue)
+    const avatarURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNTN2xMhJeimPqTtxfHfWsW6QDFi-VqRYisw&usqp=CAU"
 
     return(
-        <React.Fragment>
+        <div>
             <Navbar theme={theme} themeToggler={themeToggler}/>
-            <Grid container style={styles.grid.container}>
-                <Grid xs={12} item style={styles.grid.item}>
-                    <Carousel swipe={false} autoPlay={false} navButtonsAlwaysVisible={true}>
-                        {
-                            assets.map( (asset, i) => <ModelViewer key={i} name={asset.name} src={asset.android} iossrc={asset.ios}/> )
-                        }
-                    </Carousel>
+            <Grid container direction="row" justify="center" alignItems="center">
+                <Grid item xs={12} sm={10} md={8} item style={styles.grid.item}>
+                    <StoreDescription avatarURL={avatarURL}/>
+                    <StoreServices/>
+                    <Advantages/>
+                    <Catalogue furniture={furniture}/>
                 </Grid>
             </Grid>
-        </React.Fragment>    
+        </div>    
     )
 }
 
