7848d2989744725225ccdaea1fde887f3c2823c0
VoN <sudo.von.contact@gmail.com>
2021-05-25 22:49:13 -0500

    feat: :sparkles: Add StoreServices component where there will be a description of the services

---
+++ src/pages/AugmentedReality/Components/StoreServices/StoreServices.js
@@ -0,0 +1,49 @@
+import React from 'react'
+/* Material-ui. */
+import Grid from "@material-ui/core/Grid"
+import LocalShippingIcon from '@material-ui/icons/LocalShipping'
+import PaymentIcon from '@material-ui/icons/Payment'
+import CallIcon from '@material-ui/icons/Call'
+import EventIcon from '@material-ui/icons/Event'
+
+const styles = {
+    grid: {
+        container: {
+            backgroundColor: "white",
+            marginTop: 60,
+            marginBottom: 60,
+            padding: 15,
+            paddinBottom: 0,
+            textAlign: "center",
+            boxShadow: "0 2px 2px 0 rgb(0 0 0 / 2%), 0 3px 1px -2px rgb(0 0 0 / 4%), 0 1px 5px 0 rgb(0 0 0 / 8%)"
+        }
+    },
+    h4: {
+        marginBottom: 40
+    }
+}
+
+const StoreDescription = () =>
+    <Grid container direction="row" justify="center" alignItems="center" style={styles.grid.container}>
+        <Grid item xs={12}>
+            <h3 style={styles.h4}>Servicios</h3>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} item>
+            <LocalShippingIcon fontSize="large"/>
+            <h5>LLEVAMOS TUS MUEBLES Y TE LOS INSTALAMOS</h5>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} item>
+            <PaymentIcon fontSize="large"/>
+            <h5>ESQUEMAS FLEXIBLES DE FINANCIAMIENTO</h5>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} item>
+            <CallIcon fontSize="large"/>
+            <h5>SERVICIO POST-VENTA ESPECIALIZADO</h5>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3} item>
+            <EventIcon fontSize="large"/>
+            <h5>AGENDA TU CITA EN SUCURSAL</h5>
+        </Grid>
+    </Grid>
+
+export default StoreDescription
