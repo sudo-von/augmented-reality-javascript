b44aa1d9d6e83b6c402ac482bbf9c0ad491d5ea8
VoN <sudo.von.contact@gmail.com>
2021-05-29 13:55:26 -0500

    fix: :bug: Update assets path for the development mode

--- src/hooks/useFile.js
+++ src/hooks/useFile.js
@@ -7,7 +7,7 @@ export const useFile = ( filename ) => {
         // Format cost.
         data.cost = data.cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
         // Get absolute path based on the environment.
-        let absolute_path = process.env.NODE_ENV === 'development' ? '../../../../assets' : '../../../../augmented-reality-javascript/assets'
+        let absolute_path = process.env.NODE_ENV === 'development' ? `${process.env.PUBLIC_URL}/assets` : '../../../../augmented-reality-javascript/assets'
         data.img = `${absolute_path}/images/${data.img}`
         data.android_model = `${absolute_path}/android/${data.android_model}`
         data.ios_model = `${absolute_path}/ios/${data.ios_model}`
